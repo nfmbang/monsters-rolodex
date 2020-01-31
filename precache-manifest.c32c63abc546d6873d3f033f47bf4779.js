d082a36f3d3a54d297a5fb59e30f1d8d4f6fdefa
nfmbang <43202392+nfmbang@users.noreply.github.com>
2020-01-31 12:09:54 +0100

    Updates

---
+++ precache-manifest.c32c63abc546d6873d3f033f47bf4779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2f05ad98bd0c9f0c70f26c5e2e6e4c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2997abfd1784f7626ae5",
+    "url": "/monsters-rolodex/static/css/main.b958d037.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2997abfd1784f7626ae5",
+    "url": "/monsters-rolodex/static/js/main.05f6c7f9.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
